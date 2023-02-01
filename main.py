4b44fd015f2433ce683eb00aca0576efacb24a15
BlackStalker2008 <114511758+Black-Stalker2008@users.noreply.github.com>
2023-02-01 21:54:57 +0500

    Рабочая версия

---
+++ main.py
@@ -0,0 +1,111 @@
+import logging
+from config import TOKEN
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, ConversationHandler)
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+CHOICE, RATIONAL_ONE, RATIONAL_TWO, OPERATIONS_RATIONAL = range(4)
+
+def start(update, _):
+    update.message.reply_text(f'Привет, {update.effective_user.first_name}, это - бот калькулятор для рациональных чисел. Выберите пожалуйста команду.\n' 'Команда /cancel, чтобы прекратить разговор.\n\n')
+    update.message.reply_text('1 - Приступить к вычеслениям ; \n2 - Выйти из калькулятора \n')
+    return CHOICE
+
+
+def choice(update, context):
+    user = update.message.from_user
+    logger.info("Выбор операции: %s: %s", user.first_name, update.message.text)
+    user_choice = update.message.text
+    if user_choice in '12':
+        if user_choice == '1':
+            update.message.reply_text('Введите число. \n Первое число - это: ')
+            return RATIONAL_ONE
+        if user_choice == '2':
+            update.message.reply_text('Спасибо что воспользовались калькулятором, до свидания!')
+            return ConversationHandler.END     
+    else:
+        update.message.reply_text('Ошибка ввода. Введите цифру операции: \n 1 - для операций с рациональными числами; \n2 - для выхода \n')
+
+
+def rational_one(update, context):
+    user = update.message.from_user
+    logger.info("Пользователь ввел число: %s: %s", user.first_name, update.message.text)
+    get_rational = update.message.text
+    if get_rational.isdigit():
+        get_rational = float(get_rational)
+        context.user_data['rational_one'] = get_rational
+        update.message.reply_text('Введите второе число')
+        return RATIONAL_TWO
+
+    else:
+        update.message.reply_text('Нужно ввести число')
+
+
+def rational_two(update, context):
+    user = update.message.from_user
+    logger.info("Пользователь ввел число: %s: %s", user.first_name, update.message.text)
+    get_rational = update.message.text
+    if get_rational.isdigit():
+        get_rational = float(get_rational)
+        context.user_data['rational_two'] = get_rational
+        update.message.reply_text('Выберите действие: \n\n+ : для сложения; \n- : для вычетания; \n* : для умножения; \n/ : для деления. \n')
+        return OPERATIONS_RATIONAL
+
+
+def operatons_rational(update, context):
+    user = update.message.from_user
+    logger.info(
+        "Пользователь выбрал операцию %s: %s", user.first_name, update.message.text)
+    rational_one = context.user_data.get('rational_one')
+    rational_two = context.user_data.get('rational_two')
+    user_choice = update.message.text
+    if user_choice in '+-/*':
+        if user_choice == '+':
+            result = rational_one + rational_two
+            update.message.reply_text(f'Результат: {rational_one} + {rational_two} = {result}')
+        if user_choice == '-':
+            result = rational_one - rational_two
+            update.message.reply_text(f'Результат: {rational_one} - {rational_two} = {result}')
+        if user_choice == '*':
+            result = rational_one * rational_two
+            update.message.reply_text(f'Результат: {rational_one} * {rational_two} = {result}')
+        if user_choice == '/':
+            try:
+                result = rational_one / rational_two
+                update.message.reply_text(f'Результат: {rational_one} / {rational_two} = {result}')
+            except:
+                update.message.reply_text('Деление на ноль запрещено')        
+        return ConversationHandler.END
+    else:
+        update.message.reply_text('Ошибка ввода. Выберите действие: \n+ - для сложения; \n- - для вычетания; \n* - для умножения; \n/ - для деления. \n' )
+
+def cancel(update, _):
+    user = update.message.from_user
+    logger.info("Пользователь %s отменил разговор.", user.first_name)
+    update.message.reply_text('Спасибо что воспользовались калькулятором, до свидания!')
+    return ConversationHandler.END
+
+
+if __name__ == '__main__':
+    updater = Updater(TOKEN)
+    dispatcher = updater.dispatcher
+
+    conversation_handler = ConversationHandler(  
+    
+        entry_points=[CommandHandler('start', start)],
+    
+        states={
+            CHOICE: [MessageHandler(Filters.text, choice)],
+            RATIONAL_ONE: [MessageHandler(Filters.text, rational_one)],
+            RATIONAL_TWO: [MessageHandler(Filters.text, rational_two)],
+            OPERATIONS_RATIONAL: [MessageHandler(Filters.text, operatons_rational)],
+        },
+    
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    dispatcher.add_handler(conversation_handler)
+    print('server start')
+    updater.start_polling()
+    updater.idle()
